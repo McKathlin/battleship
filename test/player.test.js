1fc1ce16f9250a004f794307b9aff267d0466c7d
McKathlin <mckathlin@gmail.com>
2025-01-20 22:00:09 +0000

    Name Player tests in player.test.js

---
+++ test/player.test.js
@@ -0,0 +1,43 @@
+import { Player } from '../src/battleship.js';
+
+test('remembers name from init', () => {
+  // TODO
+});
+
+test('sets name with property', () => {
+  // TODO
+});
+
+test('is human by default', () => {
+  // TODO
+});
+
+test('is computer if attack AI is assigned', () => {
+  // TODO
+});
+
+test('gets a board', () => {
+  // TODO
+});
+
+test('mutually sets opponent on init', () => {
+  // TODO
+});
+
+test('mutually sets opponent with property', () => {
+  // TODO
+});
+
+test('attacks opponent board', () => {
+  // TODO
+});
+
+test('disallows attacking without opponent', () => {
+  // TODO
+});
+
+test('loses if all ships sink', () => {
+  // TODO
+});
+
+
