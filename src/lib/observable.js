6fe5ce0eacb1777046c02fea0c595de9f6c7e513
McKathlin <mckathlin@gmail.com>
2025-01-23 21:19:30 +0000

    Add Observable class

---
+++ src/lib/observable.js
@@ -0,0 +1,24 @@
+export default class Observable {
+  constructor() {
+    this._observers = [];
+  }
+
+  subscribe(onChange) {
+    this._observers.push(onChange);
+  }
+
+  unsubscribe(onChange) {
+    this._observers = this._observers.filter(
+      (observer) => observer !== onChange,
+    );
+  }
+
+  unsubscribeAll() {
+    this._observers = [];
+  }
+
+  notifyChanged(event = {}) {
+    event.sender = this;
+    this._observers.forEach((onChange) => onChange(event));
+  }
+}
