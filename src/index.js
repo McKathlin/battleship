7f76a15e361da232a87473be0b31b979877a36c9
McKathlin <mckathlin@gmail.com>
2025-01-24 19:24:24 +0000

    Display and style dummy boards

--- src/index.js
+++ src/index.js
@@ -1,3 +1,34 @@
 import "./style.css";
 
 console.log("index.js loaded!");
+
+function setupBoardView(boardNode, gameBoard) {
+  const aspectRatio = gameBoard.width / gameBoard.height;
+  boardNode.style.setProperty('aspect-ratio', aspectRatio);
+  boardNode.style.setProperty(
+    'grid-template-columns',
+    `repeat(${gameBoard.width}, 1fr)`
+  );
+  boardNode.style.setProperty(
+    'grid-template-rows',
+    `repeat(${gameBoard.height}, 1fr)`
+  );
+
+  for (let i = 0; i < gameBoard.height; i++) {
+    for (let j = 0; j < gameBoard.width; j++) {
+      let cellNode = document.createElement('div');
+      cellNode.classList.add('cell');
+      boardNode.appendChild(cellNode);
+    }
+  }
+}
+
+setupBoardView(
+  document.getElementById('player-board'),
+  { width: 10, height: 10 }
+);
+
+setupBoardView(
+  document.getElementById('opponent-board'),
+  { width: 10, height: 10 }
+);
