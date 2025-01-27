9e570d1b5aa19e898beee4b9aeded20c1046121b
McKathlin <mckathlin@gmail.com>
2025-01-27 20:29:20 +0000

    Create GridController helper class

--- src/index.js
+++ src/index.js
@@ -1,27 +1,50 @@
 import "./style.css";
 
-import { Player } from "./battleship.js";
+import { Player } from "./battleship-objects.js";
 
-function setupBoardView(boardNode, gameBoard) {
-  const aspectRatio = gameBoard.width / gameBoard.height;
-  boardNode.style.setProperty('aspect-ratio', aspectRatio);
-  boardNode.style.setProperty(
-    'grid-template-columns',
-    `repeat(${gameBoard.width}, 1fr)`
-  );
-  boardNode.style.setProperty(
-    'grid-template-rows',
-    `repeat(${gameBoard.height}, 1fr)`
-  );
+class GridController {
+  constructor(containerNode, columnCount, rowCount) {
+    this._containerNode = containerNode;
+    this._columnCount = columnCount;
+    this._rowCount = rowCount;
+    this._setupStyle();
+    this._cells = this._makeCells();
+  }
 
-  for (let i = 0; i < gameBoard.height; i++) {
-    for (let j = 0; j < gameBoard.width; j++) {
-      let cellNode = document.createElement('div');
-      cellNode.classList.add('cell');
-      boardNode.appendChild(cellNode);
+  cellNodeAt(column, row) {
+    let index = (row * this._columnCount) + column;
+    return this._cells[index];
+  }
+
+  //-- private helpers --
+
+  _setupStyle() {
+    const aspectRatio = this._columnCount / this._rowCount;
+    this._containerNode.style.setProperty('aspect-ratio', aspectRatio);
+    this._containerNode.style.setProperty(
+      'grid-template-columns',
+      `repeat(${this._columnCount}, 1fr)`
+    );
+    this._containerNode.style.setProperty(
+      'grid-template-rows',
+      `repeat(${this._rowCount}, 1fr)`
+    );
+  }
+
+  _makeCells() {
+    let cells = [];
+    for (let i = 0; i < this._rowCount; i++) {
+      for (let j = 0; j < this._columnCount; j++) {
+        let cellNode = document.createElement('div');
+        cellNode.classList.add('cell');
+        cells.push(cellNode);
+        this._containerNode.appendChild(cellNode);
+      }
     }
+    return cells;
   }
 }
+
 
 function setMessage(text) {
   document.getElementById('message').innerText = text;
@@ -30,14 +53,16 @@ function setMessage(text) {
 let player = new Player();
 let opponent = new Player({ opponent: player });
 
-setupBoardView(
+let shipGridController = new GridController(
   document.getElementById('player-board'),
-  player.board,
+  player.board.width,
+  player.board.height,
 );
 
-setupBoardView(
+let attackGridController = new GridController(
   document.getElementById('opponent-board'),
-  opponent.board,
+  opponent.board.width,
+  opponent.board.height,
 );
 
 setMessage("This game is under construction.");
